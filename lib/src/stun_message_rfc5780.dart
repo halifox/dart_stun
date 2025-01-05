7653d71097bbbc7b0e1106a63d80cfa74f85bbb3
halifox <106858061+halifox@users.noreply.github.com>
2025-01-05 18:45:17 +0800

    Added LGPLv3 license headers to source files.

--- lib/src/stun_message_rfc5780.dart
+++ lib/src/stun_message_rfc5780.dart
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2025 halifox
+ *
+ * This file is part of dart_stun.
+ *
+ * dart_stun is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * dart_stun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with dart_stun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'package:bit_buffer/bit_buffer.dart';
 import 'package:stun/src/stun_message_rfc3489.dart' as rfc3489;
 import 'package:stun/stun.dart';
