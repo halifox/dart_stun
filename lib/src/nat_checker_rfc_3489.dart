f79363c6975f62d7fe254da98c958cdf587f79be
halifox <106858061+halifox@users.noreply.github.com>
2025-01-10 21:23:03 +0800

    Added NAT behavior enum and RFC 3489/5780 explanations.

--- lib/src/nat_checker_rfc_3489.dart
+++ lib/src/nat_checker_rfc_3489.dart
@@ -1,5 +1,62 @@
-//todo
+import 'package:stun/stun.dart';
+
+/// NAT的行为类型和侦测方法是由STUN（首先在RFC3489中定义，英文全称是Simple Traversal of UDP Through NATs）协议来描述的，STUN协议包括了RFC3489、RFC5389、RFC5780、RFC5769几个系列文档。
+///
+/// 早期的STUN协议是由RFC3489（经典的STUN）来描述，其定义的NAT行为类型如下：
+///
+/// 1）Full Cone NAT - 完全锥形NAT：
+/// 所有从同一个内网IP和端口号Endpoint1发送过来的请求都会被映射成同一个外网IP和端口号Endpoint2，并且任何一个外网主机都可以通过这个映射的Endpoint2向这台内网主机发送包。也就是外网所有发往Endpoint2的数据包都会被NAT转发给Endpoint1。由于对外部请求的来源无任何限制，因此这种方式虽然足够简单，但却不安全。
+///
+/// 2）Restricted Cone NAT - 限制锥形NAT：
+/// 它是Full Cone的受限版本：所有来自同一个内网Endpoint1的请求均被NAT映射成同一个外网Endpoint2，这与Full Cone相同。但不同的是，只有当内网Endpoint1曾经发送过报文给外部主机（假设其IP地址为IP3）后，外部主机IP3发往Endpoint2的数据包才会被NAT转发给Endpoint1。这意味着，NAT设备只向内转发那些来自于当前已知的外部主机的数据包，从而保障了外部请求来源的安全性
+///
+/// 3）Port Restricted Cone NAT - 端口限制锥形NAT：
+/// 它是Restricted Cone NAT的进一步受限版，与限制锥形NAT很相似，只不过它包括端口号PORT。只有当内网Endpoint1曾经发送过报文给外部Endpoint3(包括IP和端口了)，Endpoint3发往Endpoint2的数据包才会被NAT转发给Endpoint1。端口号PORT这一要求进一步强化了对外部报文请求来源的限制，从而较Restrictd Cone更具安全性。
+///
+/// 4）Symmetric NAT - 对称NAT：
+/// 上面的1）2）3）所有的Cone NAT中，映射关系只和内网的源Endpoint1相关，只要源Endpoint1不变其都会被映射成同一个Endpoint2。而对称NAT的映射关系不只与源Endpoint1相关，还与目的Endpoint3相关。也就是源Endpoint1发往目的Endpoint30的请求被映射为Endpoint20，而源Endpoint1发往目的Endpoint31的请求，则被映射为Endpoint21了。此外，只有收到过内网主机发送的数据的外网主机才可以反过来向内网主机发送数据包。
+///
+/// 经典 STUN 定义的 NAT 行为类型是将NAT的Mapping Behavior （映射规则）和Filtering Behavior（过滤规则）统一来归类的，这样对Symmetric NAT类型的归类过于笼统，使得许多 NAT 不完全符合由它定义的类型。
+///
+/// 于是后来，RFC3489被废弃并由RFC5389来替代，在RFC5389中，将Mapping Behavior （映射规则）和Filtering Behavior（过滤规则）分开来，定义了3种Mapping Behavior （映射规则）和3种Filtering Behavior（过滤规则），一共有9种组合。
+///
+/// 为什么是3种呢？其实理由很简单，对于一个特定的内网源Endpoint1，影响其映射关系的因素不外乎就4种情况：
+///
+///     1）目的IP和目的端口PORT都无关；
+///     2）目的IP和目的端口PORT都相关；
+///     3）仅仅目的IP相关；
+///     4）仅仅目的PORT相关。
+///
+///
+/// 对于4仅仅考虑一下PORT信息有点鸡肋，基本和1差不多，于是把4去掉了。同样，对于过滤规则也一样。
+enum NatBehavior {
+  FullConeNat,
+  RestrictedConeNat,
+  PortRestrictedConeNat,
+  SymmetricNat,
+}
 
 class NatChecker {
+  String serverHost;
+  int serverPort;
+  String localIp;
+  int localPort;
 
+  NatChecker({
+    this.serverHost = "stun.hot-chilli.net",
+    this.serverPort = 3478,
+    this.localIp = "0.0.0.0",
+    this.localPort = 54320,
+  });
+
+  List<String> _localAddresses = [];
+
+  late StunClient _stunClient = StunClient.create(
+    transport: Transport.udp,
+    serverHost: serverHost,
+    serverPort: serverPort,
+    localIp: localIp,
+    localPort: localPort,
+    stunProtocol: StunProtocol.RFC3489,
+  );
 }
--- lib/src/nat_checker_rfc_5780.dart
+++ lib/src/nat_checker_rfc_5780.dart
@@ -3,6 +3,21 @@ import 'dart:io';
 
 import 'package:stun/stun.dart';
 
+/// Mapping Behavior：
+///
+/// 1）Endpoint-Independent Mapping：
+/// 对于一个内网的EndpointP，其映射的外网EndpointG是基本固定的，不会随着通信外部主机的不同而变化。
+///
+/// 2）Address and Port-Dependent Mapping：
+/// 对于一个内网的EndpointP，如果与之通信的外部为EndpointGB1，那么EndpointP就会被NAT映射成EndpointG1；
+/// 如果与之通信的外部为EndpointGB2，那么EndpointP就会被NAT映射成EndpointG2。
+/// 也就是只要之通信的外部为EndpointGB发生变化，那么映射的外网EndpointG就会变化。
+///
+/// 3）Address-Dependent Mapping：
+/// 对于一个内网的EndpointP，如果与之通信的外部为EndpointGB1，那么EndpointP就会被NAT映射成EndpointG1；
+/// 如果与之通信的外部为EndpointGB2(如果EndpointGB2的IP和EndpointGB1的相同)，
+/// 那么EndpointP同样会被NAT映射成EndpointG1，否则就会被NAT映射成EndpointG2。
+/// 也就是只要之通信的外部为EndpointGB的IP发生变化，那么映射的外网EndpointG就会变化。
 enum NatMappingBehavior {
   Block,
   EndpointIndependent,
@@ -10,6 +25,23 @@ enum NatMappingBehavior {
   AddressAndPortDependent,
 }
 
+/// Filtering Behavior：
+///
+/// 1）Endpoint-Independent Filtering：
+/// 对于这种过滤类型，NAT在在自己的一个外网EndpointG1收到数据包，只要找到与之对应的内网EndpointP1，
+/// NAT就会转发这个数据包给相应的内网EndpointP1，不管这个数据包的来源是那里。(一般来说，这样过滤规则的NAT是比较少的，因为这样的安全系数比较低)
+///
+/// 2）Address and Port-Dependent Filtering：
+/// 对于这种过滤类型，NAT在自己的一个外网EndpointG1收到来源是EndpointGA1数据包，
+/// 这个时候NAT要判断自己是否曾经通过自己的EndpointG1给EndpointGA1发送过数据包，
+/// 如果曾经发过，那么NAT就允许该数据包通过NAT并路由给内网与之对于的内网EndpointP1；
+/// 如果没发过，那么NAT会不允许该数据包通过NAT。
+///
+/// 3）Address-Dependent Filtering：
+/// 对于这种过滤类型，NAT在自己的一个外网EndpointG1收到来源是EndpointGA1数据包，
+/// 这个时候NAT要判断自己是否曾经通过自己的EndpointG1给和EndpointGA1的IP相同的机器发送过数据包(这里会忽略端口)，
+/// 如果曾经发过，那么NAT就允许该数据包通过NAT并路由给内网与之对于的内网EndpointP1；
+/// 如果没发过，那么NAT会不允许该数据包通过NAT。
 enum NatFilteringBehavior {
   Block,
   EndpointIndependent,
@@ -17,6 +49,31 @@ enum NatFilteringBehavior {
   AddressAndPortDependent,
 }
 
+/// 要进行NAT类型的侦测，需要一个具有双公网IP的服务器来协助侦测，我们称该服务器为STUN Server。假设STUN Server的双IP分别为IP_SA(125.227.152.3)和IP_SB(125.227.152.4) 监听的两个端口分别为PORT_SA(4777)和PORT_SB(4888)，客户端A的内网和端口分别为IP_CA(10.70.142.12)和PORT_CA(1234)。
+///
+/// 1）客户端A以IP_CA: PORT_CA给STUN Server的IP_SA: PORT_SA发送一个bind请求，STUN server以IP_SA: PORT_SA给客户端A的IP_CA: PORT_CA回复响应，响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA1: PORT_MCA1，STUN Server的另外一个IP地址和端口为：IP_SB: PORT_SB）。这个时候客户端判断，如果IP_CA: PORT_CA == IP_MCA1: PORT_MCA1，那么该客户端是拥有公网IP的，NAT类型侦测结束。
+///
+/// 2）客户端A以IP_CA: PORT_CA给STUN server的IP_SB: PORT_SA(相对步骤1 ip改变了)发送一个bind请求，STUN server以IP_SB: PORT_SA给客户端A的IP_CA: PORT_CA回复响应，响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA2: PORT_MCA2）。这个时候客户端判断，如果IP_MCA1: PORT_MCA1 == IP_MCA2: PORT_MCA2，那么NAT是Endpoint Independent Mapping的映射规则，也就是同样的内网地址IP_CA: PORT_CA经过这种NAT映射后的IP_M: PORT_M是固定不变的；如果IP_MCA1: PORT_MCA1 != IP_MCA2: PORT_MCA2,那么就要进行下面的第3步测试。
+///
+/// 3）客户端A以IP_CA: PORT_CA给STUN server的IP_SB: PORT_SB(相对步骤1 ip和port改变了)发送一个bind请求，STUN server以IP_SB: PORT_SB给客户端A的IP_CA: PORT_CA回复响应，响应内容大体为：（NAT映射后的IP地址和端口为：IP_MCA3: PORT_MCA3）。这个时候客户端判断，如果IP_MCA2: PORT_MCA2== IP_MCA3: PORT_MCA3，那么NAT是Address Dependent Mapping的映射规则，也就是只要是目的IP是相同的，那么同样的内网地址IP_CA: PORT_CA经过这种NAT映射后的IP_M: PORT_M是固定不变的；如果IP_MCA2: PORT_MCA2!= IP_MCA3: PORT_MCA3，那么NAT是Address and Port Dependent Mapping，只要目的IP和PORT中有一个不一样，那么同样的内网地址IP_CA: PORT_CA经过这种NAT映射后的IP_M: PORT_M是不一样的。
+///
+/// 以上三个步骤是进行Mapping Behavior的侦测，下面两个步骤是进行Filtering Behavior侦测：
+///
+/// 4）客户端A以IP_CA: PORT_CA给STUN server的IP_SA: PORT_SA发送一个bind请求（请求中带CHANGE-REQUEST attribute来要求stun server改变IP和PORT来响应），STUN server以IP_SB: PORT_SB给客户端A的IP_CA: PORT_CA回复响应。如果客户端A能收到STUN server的响应，那么NAT是Endpoint-Independent Filtering的过滤规则，也就是只要给客户端A的IP_CA: PORT_CA映射后的IP_MCA: PORT_MCA地址发送数据都能通过NAT到达客户端A的IP_CA: PORT_CA（这种过滤规则的NAT估计很少）。如果不能收到STUN server的响应，那么需要进行下面的第五步测试。
+///
+/// 5）客户端A以IP_CA: PORT_CA给STUN server的IP_SA: PORT_SA发送一个bind请求（请求中带CHANGE-REQUEST attribute来要求stun server改变PORT来响应），STUN server以IP_SA: PORT_SB给客户端A的IP_CA: PORT_CA回复响应。如果客户端A能收到STUN server的响应，NAT是Address-Dependent Filtering的过滤规则，也就是只要之前客户端A以IP_CA: PORT_CA给IP为IP_D的主机发送过数据，那么在NAT映射的有效期内，IP为IP_D的主机以任何端口给客户端A的IP_CA: PORT_CA映射后的IP_MCA: PORT_MCA地址发送数据都能通过NAT到达客户端A的IP_CA: PORT_CA；如果不能收到响应，NAT是Address and Port-Dependent Filtering的过滤规则，也即是只有之前客户端A以IP_CA: PORT_CA给目的主机的IP_D: PORT_D发送过数据，那么在NAT映射的有效期内，只有以IP_D: PORT_D给客户端A的IP_CA: PORT_CA映射后的IP_MCA: PORT_MCA地址发送数据才能通过NAT到达客户端A的IP_CA: PORT_CA。
+///
+/// 通过以上5个步骤就能完成完整的NAT类型侦测。
+///
+/// 将NAT映射规则和过滤规则组合起来就形成9中不同的NAT行为类型：
+///
+///     1）Endpoint Independent Mapping和Endpoint-Independent Filtering组合对应于RFC3489中的Full Cone NAT；
+///     2）Endpoint Independent Mapping和Address-Dependent Filtering组合对应于RFC3489中的Restricted Cone NAT；
+///     3）Endpoint Independent Mapping和Address and Port-Dependent Filtering组合对应于RFC3489中的Port Restricted Cone NAT；
+///     4）Address and Port-Dependent Mapping和Address and Port-Dependent Filtering组合是RFC3489中所说的Symmetric NAT。
+///
+///
+/// 可见RFC3489只描述了9种NAT组合行为类型中的4种。最后一个文档rfc5769，定义了一些STUN协议的测试数据用于测试STUN server的正确性。
 class NatChecker {
   String serverHost;
   int serverPort;
